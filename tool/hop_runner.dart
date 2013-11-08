1e2a77412dde40163521a529d88e569be8ef7e1c
Kevin Moore <kevin@thinkpixellab.com>
2013-11-07 23:15:11 -0300

    v0.1.5
    that’s embarrassing

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -8,7 +8,7 @@ import '../test/harness_console.dart' as test_console;
 
 import 'package:hop/src/hop_tasks_experimental.dart' as dartdoc;
 
-void main() {
+void main(List<String> args) {
   // Easy to enable hop-wide logging
   // enableScriptLogListener();
 
@@ -31,7 +31,7 @@ void main() {
 
   addTask('dart2js', createDartCompilerTask(paths, liveTypeAnalysis: true));
 
-  runHop();
+  runHop(args);
 }
 
 Future<List<String>> _getLibs() {
