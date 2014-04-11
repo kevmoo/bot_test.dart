1cd0c84ac3f75bf88f2c8b69abd1f0a9da124a08
Kevin Moore <github@j832.com>
2014-04-11 16:04:18 -0700

    support latest browser dependencies, fixed test runner

--- test/harness_console.dart
+++ test/harness_console.dart
@@ -1,16 +1,10 @@
 library harness_console;
 
 import 'package:unittest/unittest.dart';
-import 'package:unittest/vm_config.dart';
 import 'test_shared.dart' as shared;
 import 'test_dump_render_tree.dart' as drt;
 
-main() {
-  testCore(new VMConfiguration());
-}
-
-void testCore(Configuration config) {
-  unittestConfiguration = config;
+void main() {
   groupSep = ' - ';
 
   shared.main();
