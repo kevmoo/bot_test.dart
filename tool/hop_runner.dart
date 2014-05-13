eb1138fe4ea821d5af1d63c7f4cc425069abb964
Kevin Moore <github@j832.com>
2014-05-13 12:39:23 -0700

    v0.1.6+1
    Fixed docs task

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -8,12 +8,10 @@ import 'package:hop_docgen/hop_docgen.dart';
 import '../test/harness_console.dart' as test_console;
 
 void main(List<String> args) {
-  // Easy to enable hop-wide logging
-  // enableScriptLogListener();
 
   addTask('test', createUnitTestTask(test_console.main));
 
-  addTask('docs', createDocGenTask('../kev_dartdoc_viewer'));
+  addTask('docs', createDocGenTask('../compiled_dartdoc_viewer'));
 
   //
   // Analyzer
